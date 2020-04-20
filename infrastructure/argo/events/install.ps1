961bb6f195aa8b28f3dcc57f0055d8d27c2a5c7b
Fredrik Arvidsson <fredrik@fkan.se>
2020-04-20 09:28:02 +0200

    add git ssh

--- infrastructure/argo/events/install.ps1
+++ infrastructure/argo/events/install.ps1
@@ -1,1 +1,4 @@
 kubectl apply -f https://raw.githubusercontent.com/argoproj/argo-events/master/hack/k8s/manifests/installation.yaml
+
+kubectl -n argo-events create secret generic git-ssh --from-file=key=$HOME/.ssh/github
+kubectl -n argo-events create secret generic git-known-hosts --from-file=ssh_known_hosts=$HOME/.ssh/known_hosts
---
+++ infrastructure/git/generate-ssh.ps1
@@ -0,0 +1,2 @@
+choco install git.install
+ssh-keygen -t rsa -b 4096 -f $HOME/.ssh/github
