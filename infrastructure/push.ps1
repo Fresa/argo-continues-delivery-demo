dfcf9b914c61e7ff505dec0ab9dcd7faf146e766
Fredrik Arvidsson <fredrik@fkan.se>
2020-04-24 15:58:36 +0200

    use kaniko to build docker images

--- infrastructure/push.ps1
+++ infrastructure/push.ps1
@@ -1,11 +1,14 @@
 param(
     [parameter(Mandatory=$false,
         HelpMessage="The commit SHA after the push event")]
-    [String]$after = '0f4f7b2125e2b9cb8a5215335fcb14b608613f31'
+    [String]$after = '0f4f7b2125e2b9cb8a5215335fcb14b608613f31',
+    [parameter(Mandatory=$false,
+        HelpMessage="The git reference")]
+    [String]$ref = 'refs/heads/master'
 )
 
 $params = @{
-    Body = "{ ""repository"": { ""clone_url"": ""https://github.com/Fresa/argo-continues-delivery-demo.git"" }, ""after"": ""$after"" }"
+    Body = "{ ""repository"": { ""git_url"": ""git://github.com/Fresa/argo-continues-delivery-demo.git"" }, ""after"": ""$after"", ""ref"": ""$ref"" }"
     Method = "POST"
     Headers = @{ 
         'Content-Type' = 'application/json'
