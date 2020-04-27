81982b68641a89f75dc1e4113e17bfb93caaeb9d
Fredrik Arvidsson <fredrik@fkan.se>
2020-04-27 09:40:19 +0200

    argo events have broken the installation manifests, download older version for a static installation

--- infrastructure/argo/events/install.ps1
+++ infrastructure/argo/events/install.ps1
@@ -1,4 +1,5 @@
-kubectl apply -f https://raw.githubusercontent.com/argoproj/argo-events/master/hack/k8s/manifests/installation.yaml
+kubectl create namespace argo-events
+kubectl apply -f "$PSScriptRoot\install.yaml"
 
 kubectl -n argo-events create secret generic git-ssh --from-file=key=$HOME/.ssh/github
 kubectl -n argo-events create secret generic git-known-hosts --from-file=ssh_known_hosts=$HOME/.ssh/known_hosts
