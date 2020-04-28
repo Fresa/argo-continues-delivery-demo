7dae810fcd39a3dc80b4a4b09ca023ae5d65d4fa
Fredrik Arvidsson <fredrik@fkan.se>
2020-04-28 15:07:51 +0200

    update argo events install script with the renamed k8s manifests

--- infrastructure/argo/events/install.ps1
+++ infrastructure/argo/events/install.ps1
@@ -4,6 +4,6 @@ kubectl apply -f "$PSScriptRoot\install.yaml"
 kubectl -n argo-events create secret generic git-ssh --from-file=key=$HOME/.ssh/github
 kubectl -n argo-events create secret generic git-known-hosts --from-file=ssh_known_hosts=$HOME/.ssh/known_hosts
 
-kubectl apply -n argo-events -f "$PSScriptRoot\ci-webhook.yaml"
-kubectl apply -n argo-events -f "$PSScriptRoot\ci-webhook-gateway.yaml"
-kubectl apply -n argo-events -f "$PSScriptRoot\ci-webhook-sensor.yaml"
+kubectl apply -n argo-events -f "$PSScriptRoot\code-pushed-event-source.yaml"
+kubectl apply -n argo-events -f "$PSScriptRoot\code-pushed-gateway.yaml"
+kubectl apply -n argo-events -f "$PSScriptRoot\code-pushed-sensor.yaml"
