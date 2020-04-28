f7baa9ab53ab941c40450176405cdb77d06e2175
Fredrik Arvidsson <fredrik@fkan.se>
2020-04-28 09:30:57 +0200

    download install yaml for workflow and overwrite the config map to enable pns

--- infrastructure/argo/workflow/install.ps1
+++ infrastructure/argo/workflow/install.ps1
@@ -1,5 +1,5 @@
 kubectl create namespace argo
+kubectl apply -n argo -f "$PSScriptRoot\install.yaml"
 kubectl apply -n argo -f "$PSScriptRoot\workflow-controller-config-map.yaml"
-kubectl apply -n argo -f https://raw.githubusercontent.com/argoproj/argo/stable/manifests/install.yaml
 
 kubectl create rolebinding default-admin --clusterrole=admin --serviceaccount=default:default
