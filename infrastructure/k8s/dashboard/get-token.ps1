c2f2eed27afcd51c4a5c6a4b6bc2ac0fc549b1cf
Fredrik Arvidsson <fredrik@fkan.se>
2020-05-08 15:10:51 +0200

    move k8s dashboard installation into class

--- infrastructure/k8s/dashboard/get-token.ps1
+++
@@ -1,1 +0,0 @@
-kubectl -n kubernetes-dashboard describe secret $(kubectl -n kubernetes-dashboard get secret | sls admin-user | ForEach-Object { $_ -Split '\s+' } | Select -First 1)
--- infrastructure/k8s/dashboard/install.ps1
+++
@@ -1,7 +0,0 @@
-If((kubectl get svc --namespace=kube-system --field-selector=metadata.name==kubernetes-dashboard --ignore-not-found) -ne $null) {
-    Write-Verbose "'kubernetes-dashboard' is installed"
-}
-Else {
-    Write-Verbose "Installing 'kubernetes-dashboard'"
-    kubectl apply -f "$PSScriptRoot\install.yaml"
-}
