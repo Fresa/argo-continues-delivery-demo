57a74b53a572d9ac23abc929e302bdb563e22353
Fredrik Arvidsson <fredrik@fkan.se>
2020-05-08 14:51:11 +0200

    use logger for port forwarding k8s dashboard

--- infrastructure/k8s/dashboard/k8s-dashboard.psm1
+++ infrastructure/k8s/dashboard/k8s-dashboard.psm1
@@ -1,7 +1,10 @@
+Using module "..\..\log.psm1"
+
 class K8sDashboard 
 {
     [string]$Name = "KubernetesProxy"
     [int]$Port
+    [Log]$Log = [Log]::new([K8sDashboard])
 
     K8sDashboard([int]$port)
     {
@@ -15,21 +18,21 @@ class K8sDashboard
         Start-Job -Name $this.Name -InputObject $command -ScriptBlock { 
             Invoke-Expression $input
         }
-        Write-Host "Dashboard should soon be available at: http://localhost:$($this.Port)/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/#/login"
-        Write-Host "For more info: Receive-Job -Name $($this.Name)"
+        $this.Log.Info("Dashboard should soon be available at: http://localhost:$($this.Port)/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/#/login")
+        $this.Log.Info("For more info: Receive-Job -Name $($this.Name)")
     }
 
     [void] StopPortForwarding()
     {
-        Write-Host "Stopping $($this.Name)"
+        $this.Log.Info("Stopping $($this.Name)")
         if ([bool] (Get-Job -Name $this.Name -ea silentlycontinue))
          {
             Stop-Job -name $this.Name
             Remove-Job -name $this.Name
-            Write-Host "$($this.Name) stopped"
+            $this.Log.Info("$($this.Name) stopped")
         } else
         {
-            Write-Warning "Could not stop $($this.Name), the job was not found"
+            $this.Log.Warning("Could not stop $($this.Name), the job was not found")
         }
     }
 }
