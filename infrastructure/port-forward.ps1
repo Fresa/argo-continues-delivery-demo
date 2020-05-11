b1ee60cbe2e3b2947f5c1c2a4ec05d5a2e926485
Fredrik Arvidsson <fredrik@fkan.se>
2020-05-11 16:16:44 +0200

    port forwarding start jobs in parallel that requires specific context that they do not handle them selves which might cause sync issues. There is really no good way of solving that since there are no k8s context sessions, so we halt after port forwarding to wait for the service to spin up. This is not specifically taking any extra time syncing port forward jobs since it is really the installation of the services it self that takes time causing the port forward jobs to take time

--- infrastructure/port-forward.ps1
+++ infrastructure/port-forward.ps1
@@ -4,38 +4,26 @@ $clusters = [KindClusters]::new()
 $applicationClusters = $clusters.GetApplicationClusters()
 $ciCluster = $clusters.GetCICluster()
 
-# Start port forwarding
-$ciCluster.UseContext()
-$ciCluster.ArgoServer.PortForward()
-Write-Host
-$ciCluster.CodePushedGateway.PortForward()
-Write-Host
-$ciCluster.DockerRegistry.PortForward()
-Write-Host
-
-$applicationClusters | ForEach-Object {
-    $_.UseContext()
-    $_.ArgoCDServer.PortForward()
-    Write-Host
-}
-
 $clusters | ForEach-Object {
     $_.UseContext()
     $_.Dashboard.PortForward()
     Write-Host
 }
 
-# Wait until services are available
 $ciCluster.UseContext()
+$ciCluster.ArgoServer.PortForward()
 $ciCluster.ArgoServer.WaitUntilAvailable()
 Write-Host
+$ciCluster.CodePushedGateway.PortForward()
 $ciCluster.CodePushedGateway.WaitUntilAvailable()
 Write-Host
+$ciCluster.DockerRegistry.PortForward()
 $ciCluster.DockerRegistry.WaitUntilAvailable()
 Write-Host
 
 $applicationClusters | ForEach-Object {
     $_.UseContext()
+    $_.ArgoCDServer.PortForward()
     $_.ArgoCDServer.WaitUntilAvailable()
     Write-Host
 }
