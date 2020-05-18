1ad32277076b6d8304f26ae0232077cdd4bbdcda
Fredrik Arvidsson <fredrik@fkan.se>
2020-05-18 17:35:59 +0200

    input git api key as k8s secret

--- infrastructure/argo/events/argo-events.psm1
+++ infrastructure/argo/events/argo-events.psm1
@@ -12,6 +12,15 @@ class ArgoEvents {
         $this.Log.Info($(kubectl -n argo-events create secret generic git-known-hosts --from-file=ssh_known_hosts=$global:HOME/.ssh/known_hosts))
 
         # CI
+        $gitApiKeyFile = "$PSScriptRoot\git-api-key.txt"
+        if (-not ($gitApiKeyFile | Test-Path)) 
+        {
+            $gitApiKey = Read-Host -Prompt "Could not find '$gitApiKeyFile'. Please input git api key"
+            New-Item -Path $gitApiKeyFile -ItemType File 
+            Set-Content -NoNewline -Path $gitApiKeyFile -Value $gitApiKey
+        }
+
+        $this.Log.Info($(kubectl -n argo-events create secret generic git-api-key --from-file=key=$gitApiKeyFile))
         $this.Log.Info($(kubectl apply -n argo-events -f "$PSScriptRoot\code-pushed-event-source.yaml"))
         $this.Log.Info($(kubectl apply -n argo-events -f "$PSScriptRoot\code-pushed-gateway.yaml"))
         $this.Log.Info($(kubectl apply -n argo-events -f "$PSScriptRoot\code-pushed-sensor.yaml"))
